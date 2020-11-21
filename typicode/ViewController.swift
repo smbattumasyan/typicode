ffcf189a3e29407e7bac05adc45c718e384b5a20
Smbat <smbattumasyan@email.com>
2020-11-21 10:48:07 +0400

    first commit

---
+++ typicode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  typicode
+//
+//  Created by Smbat Tumasyan on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
